5264d9866118f28a9c8fbad307ef0c5503008fc9
siddkalani <siddharthkalani6@gmail.com>
2024-11-17 17:23:27 +0530

    form try 1

---
+++ api/submit-form.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+let cachedDb = null;
+
+// MongoDB connection
+async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  const uri = "mongodb+srv://admin:admin@portfolio.swr5h.mongodb.net/?retryWrites=true&w=majority&appName=portfolio";
+  const client = await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  cachedDb = client.connection;
+  return cachedDb;
+}
+
+// MongoDB schema
+const FormSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  company: String,
+  service: String,
+  message: String,
+  phone: String,
+});
+const Form = mongoose.models.Form || mongoose.model("Form", FormSchema);
+
+// API handler function
+module.exports = async (req, res) => {
+  if (req.method === "POST") {
+    try {
+      // Connect to database
+      await connectToDatabase();
+
+      // Create a new form entry
+      const formData = new Form(req.body);
+      await formData.save();
+
+      res.status(200).json({ message: "Form submitted successfully!" });
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      res.status(500).json({ error: "Failed to submit form" });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+};
